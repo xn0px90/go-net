a9736e046ea5fdfeea7952ea5667e38a7fa96833
xn0px90 <xn0px90@users.noreply.github.com>
2016-03-23 03:10:01 -0700

    Added files via upload

---
+++ IP.go
@@ -0,0 +1,25 @@
+/*IP
+ */
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s ipaddr\n", os.Args[0])
+		os.Exit(1)
+	}
+	name := os.Args[1]
+	addr := net.ParseIP(name)
+	if addr == nil {
+		fmt.Println("Invalid address")
+	} else {
+		fmt.Println("The address is", addr.String())
+	}
+	os.Exit(0)
+}
